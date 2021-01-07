ebf55be4f88275fe5b5b59247ebfa09b9bee7311
justinjeong <justin.jeong5@gmail.com>
2021-01-07 19:36:53 +0900

    add: syntax error
    even before excuting codes, typescript let you know error.

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,9 +2,24 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url)
 .then(response=>{
-  console.log(response.data);
+  const todo = response.data as Todo;
+  const ID = todo.ID;
+  const title = todo.Title;
+  const finished = todo.finished;
+
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `)
 });
 
 // terminal: tsc index.ts 
