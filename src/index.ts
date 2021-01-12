819253f97706a836f79edca2e7ebcd68597dfb90
justinjeong <justin.jeong5@gmail.com>
2021-01-12 20:00:15 +0900

    add Sorter only for nuimber[]
    using basic bubble sort only for number[]

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hi, there!')
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let end = 0; end < length; end++) {
+      for (let start = 0; start < length - end - 1; start++) {
+        if (this.collection[start] > this.collection[start + 1]){
+          const leftHand = this.collection[start];
+          this.collection[start] = this.collection[start + 1];
+          this.collection[start + 1] = leftHand;
+        }
+      }
+    }
+  }
+  printCollection(): void{
+    console.log(this.collection)
+  }
+}
+
+const sorter = new Sorter([10, 3, -5 ,0]);
+sorter.sort();
+sorter.printCollection();
+ 
