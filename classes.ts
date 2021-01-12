ca306e7e183661c77e804f8f83d8aae02d7866ac
justinjeong <justin.jeong5@gmail.com>
2021-01-12 14:53:06 +0900

    class es2015 constructor

--- classes.ts
+++ classes.ts
@@ -1,8 +1,16 @@
 class Vehicle {
+  constructor(public color: string = 'blue'){}
+  
   protected honk(): void {
     console.log('beep')
   }
+  showColor(): void {
+    console.log(this.color);
+  }
 }
+
+const vehicle = new Vehicle('orange');
+vehicle.showColor();
 
 class Car extends Vehicle {
   private drive(): void {
@@ -21,3 +29,4 @@ class Car extends Vehicle {
 const car = new Car()
 car.accelator();
 car.stop();
+car.showColor();
