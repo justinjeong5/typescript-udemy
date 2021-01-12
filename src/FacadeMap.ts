8307dc47cfa4963902b08bdf086aa42005609506
justinjeong <justin.jeong5@gmail.com>
2021-01-12 17:18:24 +0900

    add: addMarker
    interface makes addMaker highly scalable, and reusable.

--- src/FacadeMap.ts
+++ src/FacadeMap.ts
@@ -1,3 +1,10 @@
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  }
+}
+
 export class FacadeMap {
   private googleMap: google.maps.Map;
 
@@ -6,5 +13,15 @@ export class FacadeMap {
       zoom: 1,
       center: { lat: 0, lng: 0}
     });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: { 
+        lat: mappable.location.lat, 
+        lng: mappable.location.lng 
+      },
+    })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-// import { User } from './User'
-// import { Company } from './Company'
+import { User } from './User'
+import { Company } from './Company'
 import { FacadeMap } from './FacadeMap'
 
 const facadeMap = new FacadeMap('googleMap');
+const user = new User();
+const company = new Company();
+
+facadeMap.addMarker(user);
+facadeMap.addMarker(company);
