34a9adaaa03721283cdac8759b2efa02edffafa9
justinjeong <justin.jeong5@gmail.com>
2021-01-12 18:01:57 +0900

    add: implements Mappable
    implements helps us to find out where error goes off on class level.
    implements is optional, but it makes application type-powerful.

--- src/Company.ts
+++ src/Company.ts
@@ -1,6 +1,7 @@
 import faker from 'faker';
+import { Mappable } from './FacadeMap'
 
-export class Company {
+export class Company implements Mappable {
   companyName: string;
   catchPhrase: string;
   location: {
