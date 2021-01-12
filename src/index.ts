513af21d63c01edb0118228ed34693e3cb14d7f6
justinjeong <justin.jeong5@gmail.com>
2021-01-12 16:39:17 +0900

    initial commit for google maps
    basic google map.
    but expose google map directly to client extreamly dangerous,
    in that client can call any google apis which possibly leads my application to break.
    refactor for Facade design pattern does required.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { User } from './User'
-import { Company } from './Company'
+// import { User } from './User'
+// import { Company } from './Company'
 
-const user = new User();
-console.log(user);
-const company = new Company();
-console.log(company)
-
+const map = new google.maps.Map(document.querySelector('#googleMap'),{
+  zoom: 1,
+  center: { lat: 0, lng: 0}
+});
