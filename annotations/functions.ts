69a58344bfacd80ed4dc4fb4ebe6a9738b0989d7
justinjeong <justin.jeong5@gmail.com>
2021-01-12 12:24:10 +0900

    functions using destructure

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -35,3 +35,20 @@ const throwError3 = (message: string): never => {
   throw new Error(message);
 }
 
+const logWeather = (forecase: {date: Date; weather: string}): void => {
+  console.log(forecase.date)
+  console.log(forecase.weather)
+}
+
+const logWeatherES2015 = ({date, weather}: {date: Date; weather: string}): void => {
+  console.log(date)
+  console.log(weather)
+}
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+logWeather(todaysWeather)
+logWeatherES2015(todaysWeather)
