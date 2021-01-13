738949adbc26e25d2e16853ab6dd69fb28dc691b
justinjeong <justin.jeong5@gmail.com>
2021-01-13 15:36:43 +0900

    refactor: hardcoding -> enum

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,18 @@ const matches = fs.readFileSync('football.csv', {
 }).split('\n')
 .map((row: string): string[] => row.split(','))
 
+// enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
 let manUinitedWins = 0;
 
 matches.forEach(match=>{
-  if((match[1] === 'Man United' && match[5] === 'H')
-    || (match[2] === 'Man United' && match[5] === 'A')) {
+  if((match[1] === 'Man United' && match[5] === MatchResult.HomeWin)
+    || (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)) {
     manUinitedWins++;
   }
 }) 
