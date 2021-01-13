3435b6f69f02770675e327230f620ae0e3ae8401
justinjeong <justin.jeong5@gmail.com>
2021-01-13 16:12:38 +0900

    refactor: CsvFileReader for file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader'
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-}).split('\n')
-.map((row: string): string[] => row.split(','))
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 // enum - enumeration
 enum MatchResult {
@@ -14,11 +12,11 @@ enum MatchResult {
 
 let manUinitedWins = 0;
 
-matches.forEach(match=>{
+reader.data.forEach(match=>{
   if((match[1] === 'Man United' && match[5] === MatchResult.HomeWin)
     || (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)) {
     manUinitedWins++;
   }
 }) 
 
-console.log(`Man United won ${manUinitedWins} games`)
+console.log(`Man United won ${manUinitedWins} games`)  
