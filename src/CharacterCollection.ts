f1e321801ac1d6d12203fbec6665a54f4ee9f833
justinjeong <justin.jeong5@gmail.com>
2021-01-13 12:34:01 +0900

    add: sort for string
    CharactorCollection.ts using interface

---
+++ src/CharacterCollection.ts
@@ -0,0 +1,19 @@
+import { Sortable } from './Sorter'
+
+export class CharacterCollection implements Sortable {
+  constructor(public data: string) {}
+
+  get length(): number {  // make length() -> .length as if it is a kind of property.
+    return this.data.length
+  }
+  
+  swap(leftIndex: number, rightIndex: number): void {
+    const characters = this.data.split('')
+    characters[leftIndex] = [characters[rightIndex], characters[rightIndex] = characters[leftIndex]][0]
+    this.data = characters.join('');
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter'
 import { NumberCollection } from './NumberCollection'
+import { CharacterCollection } from './CharacterCollection'
 
 const numberCollection = new NumberCollection([10, 3, -5 ,0]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
 sorter.printCollection();
+
+const characterCollection = new CharacterCollection('Xaayb');
+const characterSorter = new Sorter(characterCollection);
+characterSorter.sort();
+characterSorter.printCollection();
