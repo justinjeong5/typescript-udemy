1ec2921e74cab66c4368a4b61b911a77eecf3483
justinjeong <justin.jeong5@gmail.com>
2021-01-13 15:26:34 +0900

    add: data analysis using basic ts

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,13 @@ const matches = fs.readFileSync('football.csv', {
 }).split('\n')
 .map((row: string): string[] => row.split(','))
 
-console.table(matches)
+let manUinitedWins = 0;
+
+matches.forEach(match=>{
+  if((match[1] === 'Man United' && match[5] === 'H')
+    || (match[2] === 'Man United' && match[5] === 'A')) {
+    manUinitedWins++;
+  }
+}) 
+
+console.log(`Man United won ${manUinitedWins} games`)
