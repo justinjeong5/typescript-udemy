06506a4880d7cb30ec38ccc1a9b0c9977993767d
justinjeong <justin.jeong5@gmail.com>
2021-01-14 14:23:49 +0900

    refactor: Eventing from User

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { User } from './models/User'
 
-const user = new User({ id: 1 });
+const user = new User({name: 'new record', age: 9090});
+user.events.on('change', ()=>{
+  console.log('changed!')
+})
 
-user.set({ name: 'newName#2', age: 10000 })
-user.save();
+user.events.trigger('change')
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from "axios";
+import { Eventing } from './Eventing'
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
@@ -8,11 +9,8 @@ interface UserProps {
   age?: number;
 }
 
-// typescript takes '() => void as' 'Callback has no argument and returns nothing'
-type Callback = () => void
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
+  events: Eventing = new Eventing();
 
   constructor(private data: UserProps) {}
 
@@ -22,21 +20,6 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update)
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handler = this.events[eventName] || [];
-    handler.push(callback);
-    this.events[eventName] = handler;
-  }
-
-  trigger(eventName: string): void {
-    const handler = this.events[eventName];
-    if(!handler || handler.length === 0) return;
-
-    handler.forEach(callback => {
-      callback()
-    })
   }
 
   fetch(): void {
