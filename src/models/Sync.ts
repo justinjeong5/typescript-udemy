b8fe31bbd230a230a62c6cdac79d60dbbf710c75
justinjeong <justin.jeong5@gmail.com>
2021-01-14 15:09:39 +0900

    refactor: Sync using class generic
    generig with constraint

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,13 +1,16 @@
 import axios, { AxiosPromise } from "axios";
 axios.defaults.baseURL = 'http://localhost:3000';
-import { UserProps } from './User'
 
-export class Sync {
+interface Identifiable {
+  id: number;
+}
+
+export class Sync<T extends Identifiable> {
   fetch(id: number): AxiosPromise {
     return axios.get(`/users/${id}`)
   }
 
-  save(data: UserProps): AxiosPromise {
+  save(data: T): AxiosPromise {
     const { id } = data;
     if(id) {
       return axios.put(`/users/${id}`, data)
