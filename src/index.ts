6286db302f0a0bf5d23d7a45c8629c150ab383ca
justinjeong <justin.jeong5@gmail.com>
2021-01-15 18:43:04 +0900

    fix: function -> error func

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { User } from './models/User'
 
 const user = new User({name: 'new record', age: 9090});
 
+console.log(user.get('name'));
 user.on('change', ()=>{
   console.log('changed')
 })
-
 user.trigger('change')
